688a5a5177dd46740dc645287d5bbf933b1122a5
Lyza Danger Gardner <lyza@lyza.com>
2021-04-28 15:48:15 -0400

    Add `Checkbox` basic patterns to pattern library

--- src/pattern-library/components/patterns/SharedFormPatterns.js
+++ src/pattern-library/components/patterns/SharedFormPatterns.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 import { useState } from 'preact/hooks';
 
 import { LabeledCheckbox } from '../../../';
+=======
+import { Checkbox } from '../../../';
+>>>>>>> 5aca4b7 (Add `Checkbox` basic patterns to pattern library)
 
 import {
   PatternPage,
@@ -9,6 +13,7 @@ import {
   PatternExample,
 } from '../PatternPage';
 
+<<<<<<< HEAD
 export default function SharedFormPatterns() {
   const [wantSandwich, setWantSandwich] = useState(true);
   const [wantWatermelon, setWantWatermelon] = useState(false);
@@ -39,6 +44,45 @@ export default function SharedFormPatterns() {
             >
               <code>I want a watermelon</code>
             </LabeledCheckbox>
+=======
+function gimmeSandwich(isChecked) {
+  const visibility = isChecked ? 'visible' : 'hidden';
+  const elem = /** @type HTMLElement */ (document.querySelector(
+    '#showme-sandwich'
+  ));
+  if (elem) {
+    elem.style.visibility = visibility;
+  }
+}
+
+export default function SharedFormPatterns() {
+  return (
+    <PatternPage title="Forms">
+      <Pattern title="Checkbox">
+        <span
+          id="showme-sandwich"
+          role="img"
+          aria-label="seriously, just an emoji of sandwich"
+          style="font-size: 2em; visibility:hidden"
+        >
+          🥪
+        </span>
+        <PatternExamples>
+          <PatternExample details="A simple checkbox (Check me!)">
+            <Checkbox
+              label="I want a sandwich"
+              id="sandwich"
+              name="test"
+              onChanged={isChecked => gimmeSandwich(isChecked)}
+            />
+          </PatternExample>
+          <PatternExample details="A checkbox, defaulting to checked">
+            <Checkbox
+              label="I want a sandwich"
+              name="test"
+              defaultChecked={true}
+            />
+>>>>>>> 5aca4b7 (Add `Checkbox` basic patterns to pattern library)
           </PatternExample>
         </PatternExamples>
       </Pattern>
