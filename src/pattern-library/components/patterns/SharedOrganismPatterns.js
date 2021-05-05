832ef15aa3abda9c7826613fea16fdf5762478f0
Lyza Danger Gardner <lyza@lyza.com>
2021-05-05 09:23:26 -0400

    Update pattern library molecules (card, actions), organisms (panel)

--- src/pattern-library/components/patterns/SharedOrganismPatterns.js
+++ src/pattern-library/components/patterns/SharedOrganismPatterns.js
@@ -5,19 +5,79 @@ import {
   Pattern,
 } from '../PatternPage';
 
+import { IconButton, LabeledButton } from '../../../';
+
 export default function SharedOrganismPatterns() {
   return (
     <PatternPage title="Organisms">
-      <Pattern title="Card">
+      <Pattern title="Panel">
+        <p>
+          A panel is a card with a header and affordances for a close button.
+          Use the <code>--closeable</code> modifier when using{' '}
+          <code>panel</code> with an icon-only close button.
+        </p>
         <PatternExamples>
-          <PatternExample details="A card is a framed element with internal margins and padding, and a hover shadow effect">
-            <div className="card">This is in a card.</div>
+          <PatternExample
+            details="Panel with no header"
+            style={{ maxWidth: '400px' }}
+          >
+            <div className="panel">
+              This is in a panel that has no header. A header is not required,
+              but you are encouraged to use <code>card</code> in that case. Note
+              that a <code>panel</code> will currently fill all available space.
+              The containing element has been set to{' '}
+              <code>max-width: 400px</code> here.
+            </div>
           </PatternExample>
-          <PatternExample details="A card applies vertical rhythm to its immediate-child elements: borders added here to highlight this">
-            <div className="card">
-              <div className="u-border">Child content in a card.</div>
-              <div className="u-border">Child content in a card.</div>
-              <div className="u-border">Child content in a card.</div>
+          <PatternExample details="Panel with title but no close button">
+            <div className="panel">
+              <header>
+                <h2 className="panel__title">
+                  This is a panel title in a panel header
+                </h2>
+              </header>
+              <div>
+                This is panel content. There is a header but no close
+                affordance.
+              </div>
+            </div>
+          </PatternExample>
+
+          <PatternExample details="Closeable panel (using IconButton): preferred">
+            <div className="panel--closeable">
+              <header>
+                <h2 className="panel__title">
+                  Panel title on a closeable panel
+                </h2>
+                <div className="panel__close">
+                  <IconButton icon="cancel" title="Close" />
+                </div>
+              </header>
+              <div>
+                This is panel content in a panel, using <code>--closeable</code>
+                .
+              </div>
+            </div>
+          </PatternExample>
+
+          <PatternExample details="Panel with actions">
+            <div className="panel--closeable">
+              <header>
+                <h2 className="panel__title">Panel title</h2>
+                <div className="panel__close">
+                  <IconButton icon="cancel" title="Close" />
+                </div>
+              </header>
+              <div>
+                This is panel content in a panel that also has some available
+                actions.
+              </div>
+              <div className="actions">
+                <LabeledButton title="Cancel">Cancel</LabeledButton>
+                <LabeledButton title="Try again" variant="primary">
+                  Try again
+                </LabeledButton>
+              </div>
             </div>
           </PatternExample>
         </PatternExamples>
