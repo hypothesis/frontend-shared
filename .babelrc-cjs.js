a2223b414715ffc1ff7ba7db94e2a9fb0178cee7
Robert Knight <robertknight@gmail.com>
2021-09-28 11:07:57 +0100

    Refine a comment about the CJS build

--- .babelrc-cjs.js
+++ .babelrc-cjs.js
@@ -1,9 +1,10 @@
 const baseConfig = require('./.babelrc');
 
+// Override base config to produce CommonJS instead of ESM output.
 const config = { ...baseConfig };
 const presetEnvConfig = config.presets.find(
   ([name]) => name === '@babel/preset-env'
 )[1];
-presetEnvConfig.modules = 'cjs'; // Produce CommonJS output
+presetEnvConfig.modules = 'cjs';
 
 module.exports = config;
