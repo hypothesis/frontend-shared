70c39e6434b3bad9f4a39162611a953965482561
Lyza Danger Gardner <lyza@lyza.com>
2021-11-18 13:40:34 -0500

    WIP: Optimize color palette and test CSSPurge
    When purge is enabled, portion of built stylesheet that is
    Tailwind-added styles is 4.9k uncompressed, 1.1k gzipped. This will
    grow, of course, since the only usage so far is on the color- and
    link-example pages in the pattern library, but it is respectable!

--- src/pattern-library/components/patterns/ColorFoundations.js
+++ src/pattern-library/components/patterns/ColorFoundations.js
@@ -7,36 +7,117 @@ import Library from '../Library';
 // - foreground color examples
 // - valid contrast combinations
 
-const colorConfig = tailwindConfig.theme.extend.colors ?? {};
+const colorConfig = tailwindConfig.theme.colors ?? {};
 
-function ColorSwatch({ colorKey }) {
-  const colorValue =
-    typeof colorConfig[colorKey] === 'string'
-      ? `: ${colorConfig[colorKey]}`
-      : '';
+function ColorSwatch({ className, colorKey, colorValue = '' }) {
+  const colorValueString = colorValue ? `: ${colorValue}` : '';
   return (
     <div>
-      <div className={`bg-${colorKey} h-12 mr-4`} />
+      <div className={`${className} h-12 mr-4`} />
       <p>
         <i>
           {colorKey}
-          {colorValue}
+          {colorValueString}
         </i>
       </p>
     </div>
   );
 }
 
-const colorSwatches = Object.keys(colorConfig).map(colorKey => (
-  <ColorSwatch key={colorKey} colorKey={colorKey} />
-));
-
 export default function ColorFoundations() {
   return (
     <Library.Page title="Colors">
-      <Library.Pattern title="Color swatches">
+      <Library.Pattern title="Brand">
+        <div className="grid md:grid-cols-2 gap-4">
+          <ColorSwatch
+            className="bg-brand"
+            colorKey={'brand'}
+            colorValue={colorConfig.brand.DEFAULT}
+          />
+          <ColorSwatch
+            className="bg-brand-dark"
+            colorKey={'brand-dark'}
+            colorValue={colorConfig.brand.dark}
+          />
+        </div>
+      </Library.Pattern>
+      <Library.Pattern title="Greys">
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {colorSwatches}
+          <ColorSwatch
+            className="white"
+            colorKey={'white'}
+            colorValue={colorConfig.white}
+          />
+          <ColorSwatch
+            className="bg-grey-0"
+            colorKey={'grey-0'}
+            colorValue={colorConfig['grey-0']}
+          />
+          <ColorSwatch
+            className="bg-grey-1"
+            colorKey={'grey-1'}
+            colorValue={colorConfig['grey-1']}
+          />
+          <ColorSwatch
+            className="bg-grey-2"
+            colorKey={'grey-2'}
+            colorValue={colorConfig['grey-2']}
+          />
+          <ColorSwatch
+            className="bg-grey-3"
+            colorKey={'grey-3'}
+            colorValue={colorConfig['grey-3']}
+          />
+          <ColorSwatch
+            className="bg-grey-4"
+            colorKey={'grey-4'}
+            colorValue={colorConfig['grey-4']}
+          />
+          <ColorSwatch
+            className="bg-grey-5"
+            colorKey={'grey-5'}
+            colorValue={colorConfig['grey-5']}
+          />
+          <ColorSwatch
+            className="bg-grey-6"
+            colorKey={'grey-6'}
+            colorValue={colorConfig['grey-6']}
+          />
+          <ColorSwatch
+            className="bg-grey-7"
+            colorKey={'grey-7'}
+            colorValue={colorConfig['grey-7']}
+          />
+          <ColorSwatch
+            className="bg-grey-8"
+            colorKey={'grey-8'}
+            colorValue={colorConfig['grey-8']}
+          />
+          <ColorSwatch
+            className="bg-grey-9"
+            colorKey={'grey-9'}
+            colorValue={colorConfig['grey-9']}
+          />
+        </div>
+      </Library.Pattern>
+
+      <Library.Pattern title="States">
+        <div className="grid lg:grid-cols-3 gap-4">
+          <ColorSwatch
+            className="bg-success"
+            colorKey={'success'}
+            colorValue={colorConfig.success}
+          />
+          <ColorSwatch
+            className="bg-notice"
+            colorKey={'notice'}
+            colorValue={colorConfig.notice}
+          />
+          <ColorSwatch
+            className="bg-error"
+            colorKey={'error'}
+            colorValue={colorConfig.error}
+          />
         </div>
       </Library.Pattern>
 
