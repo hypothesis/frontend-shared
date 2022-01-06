2132aa7d6d5cd5f060683bf1fe68e35a6bdd540a
Lyza Danger Gardner <lyza@lyza.com>
2022-01-06 16:11:35 -0500

    Temporarily disable preflight
    We're not quite ready to apply preflight (CSS reset) to the pattern
    library. Soon.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,7 @@ import tailwindPreset from './src/tailwind.preset.js';
 export default {
   presets: [tailwindPreset],
   content: ['./src/**/*.js', './templates/**/*.mustache'],
+  corePlugins: {
+    preflight: false,
+  },
 };
