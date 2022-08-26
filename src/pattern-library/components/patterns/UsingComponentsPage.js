a1a4c0201a3ac974ce01163b45f789f50cdbe488
Lyza Danger Gardner <lyza@lyza.com>
2022-08-26 13:20:58 -0400

    Restructure "Using components" page with Sections and edit for content

--- src/pattern-library/components/patterns/UsingComponentsPage.js
+++ src/pattern-library/components/patterns/UsingComponentsPage.js
@@ -17,57 +17,52 @@ export default function UsingComponentsPage() {
   return (
     <Library.Page
       title="Using components"
-      intro={<p>This guide applies to updated ({'"next"'}) components only.</p>}
+      intro={
+        <>
+          <p>
+            This package provides several categories of components. Which
+            category a component belongs to defines its general role and the
+            kinds of props it takes.
+          </p>
+          <ul>
+            <li>
+              <strong className="font-semibold">
+                Presentational components
+              </strong>{' '}
+              are composable components that encapsulate design patterns and UI
+              behavior
+            </li>
+            <li>
+              <strong className="font-semibold">
+                Base presentational components
+              </strong>{' '}
+              are presentational components that provide more customization
+              flexibility
+            </li>
+            <li>
+              <strong className="font-semibold">Composite components</strong>{' '}
+              combine composable components and behavior to satisfy a particular
+              use case
+            </li>
+            <li>
+              <strong className="font-semibold">Simple components</strong> are
+              opinionated, simple components that apply design patterns
+            </li>
+          </ul>
+        </>
+      }
     >
-      <Library.Pattern title="Component categories">
-        <ul>
-          <li>
-            <strong>Presentational components</strong>: Composable components
-            with customization flexibility. Example: <code>Button</code>,{' '}
-            <code>Link</code>. This is the most common category of component.
-            <ul>
-              <li>
-                <strong>Base presentational components</strong>: A type of
-                presentational component that only applies minimal, core styles
-                for the {"component's"} primary element. This adds flexibility
-                for style customization. Example: <code>ButtonBase</code>,{' '}
-                <code>LinkBase</code>.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <strong>Simple components</strong>: Opinionated, simple components
-            that apply design patterns. Example: <code>Spinner</code>.
-          </li>
-          <li>
-            <strong>Composite components</strong>: These components are
-            combinations of presentational components arranged and styled in a
-            particular way to satisfy a use case. Example:{' '}
-            <code>ScrollBox</code>.
-          </li>
-        </ul>
-      </Library.Pattern>
-      <Library.Pattern title="Component API">
-        <p>The props accepted by a component combine:</p>
-        <ul>
-          <li>
-            <strong>Common props</strong> available to all components in its
-            category
-          </li>
-          <li>
-            <strong>Specific props</strong> for the component
-          </li>
-        </ul>
-
-        <p>
-          Documentation pages for components cover the props specific to the
-          component, and call out any deviation of the component from its{' '}
-          {"category's"} common props.
-        </p>
-      </Library.Pattern>
-
-      <Library.Pattern title="Common props API">
-        <Library.Example title="Presentational components">
+      <Library.Section
+        title="Presentational components"
+        intro={
+          <p>
+            <strong>Presentational components</strong> have a common props API
+            and provide some flexibility for customization. Example:{' '}
+            <code>Button</code>, <code>Link</code>.
+          </p>
+        }
+      >
+        <Library.Pattern title="Props API">
           <p>
             <strong>Presentational components</strong> take these common props
             unless documented otherwise:
@@ -90,152 +85,207 @@ export default function UsingComponentsPage() {
 
           <p>
             Presentational components also take <strong>HTML attributes</strong>{' '}
-            applicable to their outermost element.
+            applicable to their primary element, typically the outermost element
+            rendered by the component.
           </p>
-        </Library.Example>
+        </Library.Pattern>
 
-        <Library.Example title="Base presentational components">
+        <Library.Pattern title="Base presentational components">
           <p>
-            <strong>Base presentational components</strong> extend
-            presentational component props with an optional boolean{' '}
-            <code>unstyled</code> prop. When set, the component will have no
-            styling at all, allowing for complete customization.
+            <strong>Base presentational components</strong> are similar to
+            presentational components, but allow more extensive customization.
           </p>
-        </Library.Example>
+          <p>
+            Example: <code>ButtonBase</code>, <code>LinkBase</code>.
+          </p>
 
-        <Library.Example title="Composite components">
+          <p>
+            By default, base presentational components apply some core styling.{' '}
+            <strong>Base styling might include</strong> things like basic layout
+            (e.g. establishing a <code>flex</code> layout), focus rings,
+            establishing transition <em>properties</em> or preventing text from
+            wrapping inside a button. Base presentational components can be
+            configured to disable all base styles.
+          </p>
+          <p>
+            However, <strong>base styling does not include</strong> things like
+            colors, dimensions (padding, spacing, gaps, margins or sizing), text
+            styling (size, style, weight, decoration) or borders.
+          </p>
+          <Library.Example title="Props API">
+            <p>
+              <strong>Base presentational components</strong> extend
+              presentational component props with an optional boolean{' '}
+              <code>unstyled</code> prop. When set, the component will have no
+              styling at all, allowing for complete customization.
+            </p>
+          </Library.Example>
+        </Library.Pattern>
+      </Library.Section>
+
+      <Library.Section
+        title="Composite components"
+        intro={
+          <>
+            <p>
+              <strong>Composite components</strong> compose and style
+              presentational components in a particular way to satisfy a use
+              case and sometimes support more nuanced or advanced behavior.
+            </p>
+            <p>
+              Example: <code>ScrollBox</code>, <code>Panel</code>.
+            </p>
+          </>
+        }
+      >
+        <Library.Pattern title="Props API">
           <p>
             <strong>Composite components</strong> accept the same common props
             as presentational components with the exception of{' '}
             <code>classes</code>.
           </p>
-        </Library.Example>
+        </Library.Pattern>
+      </Library.Section>
 
-        <Library.Example title="Simple components">
-          <p>
-            <strong>Simple components</strong> take only documented,
-            component-specific props.
-          </p>
-        </Library.Example>
-      </Library.Pattern>
+      <Library.Section
+        title="Simple components"
+        intro={
+          <>
+            <p>
+              <strong>Simple components</strong> are opinionated, simple
+              components that apply design patterns. They provide minimal, if
+              any, options for customization.
+            </p>
+            <p>
+              Example: <code>Spinner</code>.
+            </p>
+          </>
+        }
+      >
+        <Library.Pattern title="Props API">
+          Simple components only accept component-specific props.
+        </Library.Pattern>
+      </Library.Section>
 
-      <Library.Pattern title="Other prop conventions">
-        <Library.Example title="variant">
-          <p>
-            Many components provide stylistic <strong>variants</strong>.
-            Variants are mutually exclusive and may affect several different
-            aspects of styling.
-          </p>
-          <p>
-            In contrast, other props like <code>underline</code> or{' '}
-            <code>size</code> impact a single aspect of styling and may be
-            combined in various ways.
-          </p>
-          <Library.Demo
-            title="Card variants, affecting shadows and hover effects"
-            withSource
-          >
-            <Card variant="raised">
-              <CardContent>
-                This is content inside of a <code>Card</code> with the default,{' '}
-                <code>raised</code> <code>variant</code>.
-              </CardContent>
-            </Card>
+      <Library.Section title="Working with components">
+        <Library.Pattern title="Common prop conventions">
+          <Library.Example title="variant">
+            <p>
+              Many components provide stylistic <strong>variants</strong>.
+              Variants are mutually exclusive and may affect several different
+              aspects of styling.
+            </p>
+            <p>
+              In contrast, other props like <code>underline</code> or{' '}
+              <code>size</code> impact a single aspect of styling and may be
+              combined in various ways.
+            </p>
+            <Library.Demo
+              title="Card variants, affecting shadows and hover effects"
+              withSource
+            >
+              <Card variant="raised">
+                <CardContent>
+                  This is content inside of a <code>Card</code> with the
+                  default, <code>raised</code> <code>variant</code>.
+                </CardContent>
+              </Card>
 
-            <Card variant="flat">
-              <CardContent>
-                This is content inside of a <code>Card</code> with the{' '}
-                <code>flat</code> <code>variant</code>.
-              </CardContent>
-            </Card>
-          </Library.Demo>
-        </Library.Example>
+              <Card variant="flat">
+                <CardContent>
+                  This is content inside of a <code>Card</code> with the{' '}
+                  <code>flat</code> <code>variant</code>.
+                </CardContent>
+              </Card>
+            </Library.Demo>
+          </Library.Example>
 
-        <Library.Example title="size">
-          <p>
-            <code>size</code> takes size abbreviations (strings) as values and
-            impacts spacing and padding. Typically, accepted values are{' '}
-            <code>{"'sm', 'md', 'lg'"}</code>. Components generally inherit font
-            sizing, unless documented otherwise.
-          </p>
-          <Library.Demo withSource>
-            <Button icon={EditIcon} size="sm">
-              Small (sm)
-            </Button>
-            <Button icon={ReplyIcon} size="md">
-              Medium (md, default)
-            </Button>
-            <Button icon={CheckIcon} size="lg">
-              Large (lg)
-            </Button>
-          </Library.Demo>
-        </Library.Example>
+          <Library.Example title="size">
+            <p>
+              <code>size</code> takes size abbreviations (strings) as values and
+              impacts spacing and padding. Typically, accepted values are{' '}
+              <code>{"'sm', 'md', 'lg'"}</code>. Components generally inherit
+              font sizing, unless documented otherwise.
+            </p>
+            <Library.Demo withSource>
+              <Button icon={EditIcon} size="sm">
+                Small (sm)
+              </Button>
+              <Button icon={ReplyIcon} size="md">
+                Medium (md, default)
+              </Button>
+              <Button icon={CheckIcon} size="lg">
+                Large (lg)
+              </Button>
+            </Library.Demo>
+          </Library.Example>
 
-        <Library.Example title="Boolean props">
-          <p>
-            Boolean props are named for their truthy state. For example,{' '}
-            <code>ScrollBox</code> has borders by default. Thus the associated
-            boolean prop to disable them is named <code>borderless</code> (not{' '}
-            <code>border={'{false}'}</code>).
-          </p>
-          <Library.Demo title="Turning off borders on a ScrollBox" withSource>
-            <div className="w-[280px] h-[200px]">
-              <ScrollBox borderless>
-                <ul>
-                  <SampleListElements />
-                </ul>
-              </ScrollBox>
-            </div>
-          </Library.Demo>
-        </Library.Example>
-      </Library.Pattern>
+          <Library.Example title="Boolean props">
+            <p>
+              Boolean props are named for their truthy state. For example,{' '}
+              <code>ScrollBox</code> has borders by default. Thus the associated
+              boolean prop to disable them is named <code>borderless</code> (not{' '}
+              <code>border={'{false}'}</code>).
+            </p>
+            <Library.Demo title="Turning off borders on a ScrollBox" withSource>
+              <div className="w-[280px] h-[200px]">
+                <ScrollBox borderless>
+                  <ul>
+                    <SampleListElements />
+                  </ul>
+                </ScrollBox>
+              </div>
+            </Library.Demo>
+          </Library.Example>
+        </Library.Pattern>
 
-      <Library.Pattern title="Inspecting and debugging components">
-        <p>
-          To aid with in-browser inspecting, all components add a{' '}
-          <code>data-component</code> and, sometimes, a{' '}
-          <code>data-composite-component</code> attribute to their outermost
-          elements.
-        </p>
-        <Library.Example title="data-component">
+        <Library.Pattern title="Inspecting and debugging components">
           <p>
-            Presentational and simple components set <code>data-component</code>{' '}
-            only.
+            To aid with in-browser inspecting, all components add a{' '}
+            <code>data-component</code> and, sometimes, a{' '}
+            <code>data-composite-component</code> attribute to their outermost
+            elements.
           </p>
-          <Next.Code
-            content={
-              <div
-                /* eslint-disable-next-line react/no-unknown-property */
-                class="rounded-sm border bg-white shadow hover:shadow-md w-full"
-                data-component="Card"
-              />
-            }
-            size="sm"
-            title="Rendered outer div for Card"
-          />
-        </Library.Example>
+          <Library.Example title="data-component">
+            <p>
+              Presentational and simple components set{' '}
+              <code>data-component</code> only.
+            </p>
+            <Next.Code
+              content={
+                <div
+                  /* eslint-disable-next-line react/no-unknown-property */
+                  class="rounded-sm border bg-white shadow hover:shadow-md w-full"
+                  data-component="Card"
+                />
+              }
+              size="sm"
+              title="Rendered outer div for Card"
+            />
+          </Library.Example>
 
-        <Library.Example title="data-composite-component">
-          <p>
-            Composite components set <code>data-composite-component</code>. For
-            example, the outermost element of <code>Panel</code> is both a{' '}
-            <code>Card</code> (<code>data-component</code>) and a{' '}
-            <code>Panel</code> (<code>data-composite-component</code>).
-          </p>
-          <Next.Code
-            content={
-              <div
-                /* eslint-disable-next-line react/no-unknown-property */
-                class="rounded-sm border bg-white shadow hover:shadow-md w-full"
-                data-component="Card"
-                data-composite-component="Panel"
-              />
-            }
-            size="sm"
-            title="Rendered outer div for Panel"
-          />
-        </Library.Example>
-      </Library.Pattern>
+          <Library.Example title="data-composite-component">
+            <p>
+              Composite components set <code>data-composite-component</code>.
+              For example, the outermost element of <code>Panel</code> is both a{' '}
+              <code>Card</code> (<code>data-component</code>) and a{' '}
+              <code>Panel</code> (<code>data-composite-component</code>).
+            </p>
+            <Next.Code
+              content={
+                <div
+                  /* eslint-disable-next-line react/no-unknown-property */
+                  class="rounded-sm border bg-white shadow hover:shadow-md w-full"
+                  data-component="Card"
+                  data-composite-component="Panel"
+                />
+              }
+              size="sm"
+              title="Rendered outer div for Panel"
+            />
+          </Library.Example>
+        </Library.Pattern>
+      </Library.Section>
     </Library.Page>
   );
 }
