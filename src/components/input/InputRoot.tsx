8160c6c85675dc2e04fa758b12c09d092bf515d0
Lyza Danger Gardner <lyza@lyza.com>
2023-02-03 11:36:43 -0500

    Extract `InputRoot` component
    Create a new `InputRoot` component. This generalized component, not
    part of the public API, can apply consistent design patterns to various
    input element types.

---
+++ src/components/input/InputRoot.tsx
@@ -0,0 +1,62 @@
+import classnames from 'classnames';
+import type { JSX } from 'preact';
+
+import type { PresentationalProps } from '../../types';
+import { downcastRef } from '../../util/typing';
+
+import { inputGroupStyles } from './InputGroup';
+
+type RootComponentProps = {
+  element?: 'input' | 'select';
+  hasError?: boolean;
+};
+
+export type InputRootProps = PresentationalProps &
+  RootComponentProps &
+  (
+    | Omit<JSX.HTMLAttributes<HTMLInputElement>, 'size' | 'icon'>
+    | Omit<JSX.HTMLAttributes<HTMLSelectElement>, 'size' | 'icon'>
+  );
+
+/**
+ * Root component for various input types that applies a consistent design
+ * pattern.
+ */
+const InputRootNext = function InputRoot({
+  element: Element = 'input',
+  children,
+  classes,
+  elementRef,
+  hasError,
+
+  ...htmlAttributes
+}: InputRootProps) {
+  // @ts-expect-error - "aria-label" is missing from HTMLInputAttributes
+  if (!htmlAttributes.id && !htmlAttributes['aria-label']) {
+    console.warn(
+      '`Input` component should have either an `id` or an `aria-label` attribute'
+    );
+  }
+
+  return (
+    <Element
+      data-component="InputRoot"
+      {...htmlAttributes}
+      // @ts-ignore-next
+      ref={downcastRef(elementRef)}
+      className={classnames(
+        'focus-visible-ring ring-inset border rounded-sm w-full p-2',
+        'bg-grey-0 focus:bg-white disabled:bg-grey-1',
+        'placeholder:text-color-grey-5 disabled:placeholder:color-grey-6',
+        { 'ring-inset ring-2 ring-red-error': hasError },
+        // Adapt styles when this component is inside an InputGroup
+        inputGroupStyles,
+        classes
+      )}
+    >
+      {children}
+    </Element>
+  );
+};
+
+export default InputRootNext;
