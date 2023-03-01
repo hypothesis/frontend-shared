3d8d3f243bd6a67547548b7d7e0c6df376637404
Lyza Danger Gardner <lyza@lyza.com>
2023-03-01 08:55:51 -0500

    Convert package types to TS

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+import type { ComponentChildren, FunctionComponent, JSX, Ref } from 'preact';
+
+/** Props common to components that play a primarily presentational role. */
+export type PresentationalProps = {
+  children?: ComponentChildren;
+  /** Optional extra CSS classes appended to the component's className */
+  classes?: string | string[];
+  /** Use `classes` or props instead */
+  className?: never;
+  /** Ref associated with components outermost or primary element */
+  elementRef?: Ref<HTMLElement | undefined>;
+};
+
+/**
+ * Props common to components that are opinionated compositions of other
+ * components.
+ */
+export type CompositeProps = Omit<PresentationalProps, 'classes'>;
+
+/** Props common to Base components */
+export type BaseProps = PresentationalProps & { unstyled?: boolean };
+
+/**
+ * A type describing any of the standalone icon components, which take any
+ * valid `<svg>` element attribute as props.
+ */
+export type IconComponent = FunctionComponent<JSX.SVGAttributes<SVGSVGElement>>;
