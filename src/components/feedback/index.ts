03480bdeeb9480466efe35af01e1a01de039353c
Lyza Danger Gardner <lyza@lyza.com>
2023-03-01 13:26:21 -0500

    Export types for feedback components

--- src/components/feedback/index.ts
+++ src/components/feedback/index.ts
@@ -1,3 +1,7 @@
 export { default as Modal } from './Modal';
 export { default as Spinner } from './Spinner';
 export { default as SpinnerOverlay } from './SpinnerOverlay';
+
+export type { ModalProps } from './Modal';
+export type { SpinnerProps } from './Spinner';
+export type { SpinnerOverlayProps } from './SpinnerOverlay';
