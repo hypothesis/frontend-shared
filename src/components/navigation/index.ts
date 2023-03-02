1739acae19a9e09014f7ff0b2dc6476ecef3e5be
Lyza Danger Gardner <lyza@lyza.com>
2023-03-02 08:48:42 -0500

    Export props types for navigation components

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,6 +1,13 @@
-export { default as PointerButton } from './PointerButton';
 export { default as Link } from './Link';
 export { default as LinkBase } from './LinkBase';
 export { default as LinkButton } from './LinkButton';
+export { default as PointerButton } from './PointerButton';
 export { default as Tab } from './Tab';
 export { default as TabList } from './TabList';
+
+export type { LinkProps } from './Link';
+export type { LinkBaseProps } from './LinkBase';
+export type { LinkButtonProps } from './LinkButton';
+export type { PointerButtonProps } from './PointerButton';
+export type { TabProps } from './Tab';
+export type { TabListProps } from './TabList';
