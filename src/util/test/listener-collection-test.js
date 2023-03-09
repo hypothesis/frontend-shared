dd1811bee46964aee1ef557b852c9704ac0e64d4
Lyza Danger Gardner <lyza@lyza.com>
2023-03-09 12:47:33 -0500

    Make sure to pass `options` when removing event listeners
    It's necessary to pass the same `options` to `removeEventListener` as
    `addEventListener`, or the listener will not be removed.

--- src/util/test/listener-collection-test.js
+++ src/util/test/listener-collection-test.js
@@ -25,13 +25,24 @@ describe('ListenerCollection', () => {
     it('unregisters the specified listener', () => {
       const listener1 = sinon.stub();
       const listener2 = sinon.stub();
+      const listener3 = sinon.stub();
       listeners.add(window, 'resize', listener1);
       const listenerId = listeners.add(window, 'resize', listener2);
+      const capturePhaseListenerId = listeners.add(
+        window,
+        'resize',
+        listener3,
+        {
+          capture: true,
+        }
+      );
       listeners.remove(listenerId);
+      listeners.remove(capturePhaseListenerId);
 
       window.dispatchEvent(new Event('resize'));
       assert.calledOnce(listener1);
       assert.notCalled(listener2);
+      assert.notCalled(listener3);
     });
   });
 
@@ -39,13 +50,18 @@ describe('ListenerCollection', () => {
     it('unregisters all event listeners', () => {
       const listener1 = sinon.stub();
       const listener2 = sinon.stub();
+      const listener3 = sinon.stub();
       listeners.add(window, 'resize', listener1);
       listeners.add(window, 'resize', listener2);
+      listeners.add(window, 'resize', listener3, {
+        capture: true,
+      });
       listeners.removeAll();
 
       window.dispatchEvent(new Event('resize'));
       assert.notCalled(listener1);
       assert.notCalled(listener2);
+      assert.notCalled(listener3);
     });
   });
 });
