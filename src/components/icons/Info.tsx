0bd225c5b41c817b64fb27a3a3393f7f460f76e3
Lyza Danger Gardner <lyza@lyza.com>
2023-04-18 09:34:43 -0400

    Add Pin, Info icons to support UI prototyping

---
+++ src/components/icons/Info.tsx
@@ -0,0 +1,26 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type InfoIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from info.svg
+ */
+export default function InfoIcon(props: InfoIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      data-component="InfoIcon"
+      {...props}
+    >
+      <path fill="currentColor" d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20z" />
+      <path
+        fill="#fff"
+        d="M11 12a1 1 0 0 1 0-2h2a1 1 0 0 1 .96 1.27L12.33 17H13a1 1 0 0 1 0 2h-2a1 1 0 0 1-.96-1.27L11.67 12H11zm2-4a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+      />
+    </svg>
+  );
+}
