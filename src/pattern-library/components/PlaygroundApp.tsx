2670c7d82b95d45455900acd97a97b6ce867ec04
Lyza Danger Gardner <lyza@lyza.com>
2023-04-18 09:34:43 -0400

    Establish a nav section for prototypes and a directory to hold them

--- src/pattern-library/components/PlaygroundApp.tsx
+++ src/pattern-library/components/PlaygroundApp.tsx
@@ -153,6 +153,8 @@ export default function PlaygroundApp({
     </>
   );
 
+  const prototypeRoutes = getRoutes('prototype');
+
   const groupKeys = Object.keys(componentGroups) as Array<
     keyof typeof componentGroups
   >;
@@ -193,6 +195,17 @@ export default function PlaygroundApp({
                   </NavSection>
                 );
               })}
+
+              {prototypeRoutes.length > 0 && (
+                <>
+                  <NavHeader>Prototypes</NavHeader>
+                  <NavList>
+                    {prototypeRoutes.map(route => (
+                      <NavLink key={route.title} route={route} />
+                    ))}
+                  </NavList>
+                </>
+              )}
 
               {extraRoutes.length > 0 && (
                 <>
