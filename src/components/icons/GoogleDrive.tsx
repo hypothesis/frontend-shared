83a9dbb8f1181038ff66998dc3767a5b25ae9ecc
Lyza Danger Gardner <lyza@lyza.com>
2023-04-27 14:04:23 -0400

    UI Tennis: LMS content selection, round 2

---
+++ src/components/icons/GoogleDrive.tsx
@@ -0,0 +1,23 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type GoogleDriveIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from google-drive.svg
+ */
+export default function GoogleDriveIcon(props: GoogleDriveIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      fill="currentColor"
+      data-name="Layer 1"
+      viewBox="0 0 24 24"
+      data-component="GoogleDriveIcon"
+      {...props}
+    >
+      <path d="m21.423 13.888-6.09-10.55H8.668l6.09 10.55ZM8.09 4.338 2 14.888l3.334 5.774 6.089-10.55Zm1.733 10.55-3.333 5.774h12.178L22 14.887Z" />
+    </svg>
+  );
+}
