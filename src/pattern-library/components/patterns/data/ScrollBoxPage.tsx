edf75d28260053ebb03558535eb2494aed3aab2f
Lyza Danger Gardner <lyza@lyza.com>
2023-05-19 14:27:06 -0400

    Reformat `ScrollBox` page documentation

--- src/pattern-library/components/patterns/data/ScrollBoxPage.tsx
+++ src/pattern-library/components/patterns/data/ScrollBoxPage.tsx
@@ -30,26 +30,34 @@ export default function ScrollBoxPage() {
       >
         <Library.Pattern>
           <Library.Usage componentName="ScrollBox" />
-          <Library.Example title="Basic ScrollBox">
-            <Library.Demo withSource>
-              <div className="w-[280px] h-[200px]">
-                <ScrollBox>
-                  <ul>
-                    <SampleListElements />
-                  </ul>
-                </ScrollBox>
-              </div>
-            </Library.Demo>
-          </Library.Example>
+          <Library.Demo title="Basic Scrollbox" withSource>
+            <div className="w-[280px] h-[200px]">
+              <ScrollBox>
+                <ul>
+                  <SampleListElements />
+                </ul>
+              </ScrollBox>
+            </div>
+          </Library.Demo>
         </Library.Pattern>
 
-        <Library.Pattern title="Props">
+        <Library.Pattern title="Component API">
+          <p>
+            <code>ScrollBox</code> takes all standard props from the composite
+            component API.
+          </p>
           <Library.Example title="borderless">
-            <p>
-              <code>ScrollBox</code> applies a border to the outer{' '}
-              <code>ScrollContainer</code> by default. This can be disabled with
-              the <code>borderless</code> boolean prop.
-            </p>
+            <Library.Info>
+              <Library.InfoItem label="description">
+                Disable the border on the <code>ScrollBox</code>
+              </Library.InfoItem>
+              <Library.InfoItem label="type">
+                <code>boolean</code>
+              </Library.InfoItem>
+              <Library.InfoItem label="default">
+                <code>false</code>
+              </Library.InfoItem>
+            </Library.Info>
             <Library.Demo title="Turning off borders" withSource>
               <div className="w-[280px] h-[200px]">
                 <ScrollBox borderless>
@@ -64,7 +72,7 @@ export default function ScrollBoxPage() {
       </Library.Section>
 
       <Library.Section
-        title="Customizing scrolling"
+        title="Working with scrolling components"
         intro={
           <p>
             <code>Scroll</code> and its allies allow for more customization
@@ -73,7 +81,7 @@ export default function ScrollBoxPage() {
           </p>
         }
       >
-        <Library.Pattern title="Working with Scroll components">
+        <Library.Pattern title="Composing Scroll components">
           <Library.Example>
             <p>
               Sizing constraints are dictated by the immediate parent container
@@ -113,7 +121,7 @@ export default function ScrollBoxPage() {
         <Library.Pattern>
           <Library.Usage componentName="Scroll" />
           <Library.Example>
-            <Library.Demo title="Using Scroll by itself" withSource>
+            <Library.Demo title="Basic Scroll" withSource>
               <div className="w-[320px] h-[200px]">
                 <Scroll>
                   <ul>
@@ -125,23 +133,26 @@ export default function ScrollBoxPage() {
           </Library.Example>
         </Library.Pattern>
 
-        <Library.Pattern title="Props">
+        <Library.Pattern title="Component API">
+          <p>
+            <code>Scroll</code> takes all standard props from the presentational
+            component API.
+          </p>
           <Library.Example title="variant">
-            <p>
-              <code>Scroll</code>
-              {"'s"} <code>raised</code> variant (default) renders CSS shadows
-              to hint that content is scrollable. These can be disabled by using
-              the <code>flat</code> variant.
-            </p>
-            <Library.Demo title="variant:'raised' (default)" withSource>
-              <div className="w-[320px] h-[200px]">
-                <Scroll variant="raised">
-                  <ul>
-                    <SampleListElements />
-                  </ul>
-                </Scroll>
-              </div>
-            </Library.Demo>
+            <Library.Info>
+              <Library.InfoItem label="description">
+                <code>Scroll</code>
+                {"'s"} default <code>{"'raised'"}</code> <code>variant</code>{' '}
+                renders CSS shadows to hint that content is scrollable. These
+                can be disabled by using the <code>flat</code> variant.
+              </Library.InfoItem>
+              <Library.InfoItem label="type">
+                <code>{`'raised' | 'flat'`}</code>
+              </Library.InfoItem>
+              <Library.InfoItem label="default">
+                <code>{'raised'}</code>
+              </Library.InfoItem>
+            </Library.Info>
 
             <Library.Demo title="variant:'flat'" withSource>
               <div className="w-[320px] h-[200px]">
@@ -204,31 +215,37 @@ export default function ScrollBoxPage() {
         <Library.Pattern>
           <Library.Usage componentName="ScrollContainer" />
 
-          <Library.Example>
-            <Library.Demo title="Using ScrollContainer" withSource>
-              <div className="w-[320px] h-[200px]">
-                <ScrollContainer>
-                  <div className="p-2 border-b">Non-scrolling content here</div>
-                  <Scroll>
-                    <ScrollContent>
-                      <ul>
-                        <SampleListElements />
-                      </ul>
-                    </ScrollContent>
-                  </Scroll>
-                  <div className="p-2 border-t">More non-scrolling content</div>
-                </ScrollContainer>
-              </div>
-            </Library.Demo>
-          </Library.Example>
+          <Library.Demo title="Basic ScrollContainer" withSource>
+            <div className="w-[320px] h-[200px]">
+              <ScrollContainer>
+                <div className="p-2 border-b">Non-scrolling content here</div>
+                <Scroll>
+                  <ScrollContent>
+                    <ul>
+                      <SampleListElements />
+                    </ul>
+                  </ScrollContent>
+                </Scroll>
+                <div className="p-2 border-t">More non-scrolling content</div>
+              </ScrollContainer>
+            </div>
+          </Library.Demo>
         </Library.Pattern>
 
-        <Library.Pattern title="Props">
+        <Library.Pattern title="Component API">
           <Library.Example title="borderless">
-            <p>
-              Turn off <code>ScrollContainer</code> borders with the{' '}
-              <code>borderless</code> boolean prop.
-            </p>
+            <Library.Info>
+              <Library.InfoItem label="description">
+                Disable the border on the <code>ScrollContainer</code>
+              </Library.InfoItem>
+              <Library.InfoItem label="type">
+                <code>boolean</code>
+              </Library.InfoItem>
+              <Library.InfoItem label="default">
+                <code>false</code>
+              </Library.InfoItem>
+            </Library.Info>
+
             <Library.Demo withSource>
               <div className="w-[320px] h-[200px]">
                 <ScrollContainer borderless>
