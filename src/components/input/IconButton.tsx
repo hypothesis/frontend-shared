0c4e07d0b5f4f93259188d831287e32f721f0f5d
Lyza Danger Gardner <lyza@lyza.com>
2023-05-22 14:37:31 -0400

    Add styling API support to `IconButton`

--- src/components/input/IconButton.tsx
+++ src/components/input/IconButton.tsx
@@ -2,30 +2,34 @@ import classnames from 'classnames';
 
 import type { IconComponent, PresentationalProps } from '../../types';
 import { downcastRef } from '../../util/typing';
-import ButtonBase from './ButtonBase';
-import type { ButtonCommonProps, HTMLButtonAttributes } from './ButtonBase';
+import type { ButtonProps } from './Button';
+import Button from './ButtonBase';
 import { inputGroupStyles } from './InputGroup';
 
 type ComponentProps = {
   icon?: IconComponent;
-  size?: 'xs' | 'sm' | 'md' | 'lg';
 
   /**
    * Disable minimum tap target sizing for touch devices. This may be necessary
    * in legacy patterns where there isn't enough room in the interface for these
    * larger dimensions.
+   *
+   * @deprecated Set `size` to `'custom'` instead
    */
   disableTouchSizing?: boolean;
-  variant?: 'primary' | 'secondary' | 'dark';
 
   /** Required for `IconButton` as there is no text label */
   title: string;
+
+  // Styling API
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'custom';
+  unstyled?: boolean;
+  variant?: 'primary' | 'secondary' | 'dark' | 'custom';
 };
 
 export type IconButtonProps = PresentationalProps &
-  ButtonCommonProps &
-  ComponentProps &
-  HTMLButtonAttributes;
+  Omit<ButtonProps, 'title' | 'size' | 'variant'> &
+  ComponentProps;
 
 /**
  * Render a button that only contains an icon.
@@ -43,17 +47,28 @@ const IconButton = function IconButton({
   size = 'md',
   title,
   variant = 'secondary',
+  unstyled = false,
 
   ...htmlAttributes
 }: IconButtonProps) {
+  const styled = !unstyled;
+  const themed = styled && variant !== 'custom';
+  const sized = styled && size !== 'custom';
+
   return (
-    <ButtonBase
+    <Button
       data-component="IconButton"
       {...htmlAttributes}
       classes={classnames(
-        'focus-visible-ring transition-colors whitespace-nowrap flex items-center',
-        'justify-center gap-x-2 rounded-sm',
-        {
+        styled && {
+          'focus-visible-ring transition-colors rounded-sm whitespace-nowrap':
+            true,
+          'flex items-center justify-center': true,
+          // Adapt styling when in an InputGroup
+        },
+        // Adapt styles when in an InputGroup
+        styled && inputGroupStyles,
+        themed && {
           // variant
           'text-grey-7 bg-transparent enabled:hover:text-grey-9 aria-pressed:text-brand aria-expanded:text-brand':
             variant === 'secondary', //default
@@ -61,19 +76,17 @@ const IconButton = function IconButton({
             variant === 'primary',
           'text-grey-7 bg-grey-2 enabled:hover:text-grey-9 enabled:hover:bg-grey-3 disabled:text-grey-5 aria-pressed:bg-grey-3 aria-expanded:bg-grey-3':
             variant === 'dark',
-
-          // size
+        },
+        sized && {
+          'gap-x-2': true,
           'p-2': size === 'md', // Default
           'p-1': size === 'xs',
           'p-1.5': size === 'sm',
           'p-2.5': size === 'lg',
 
-          // Responsive
           'touch:min-w-touch-minimum touch:min-h-touch-minimum':
             !disableTouchSizing,
         },
-        // Adapt styling when this component is inside an InputGroup
-        inputGroupStyles,
         classes
       )}
       elementRef={downcastRef(elementRef)}
@@ -84,7 +97,7 @@ const IconButton = function IconButton({
     >
       {Icon && <Icon className="w-em h-em" />}
       {children}
-    </ButtonBase>
+    </Button>
   );
 };
 
