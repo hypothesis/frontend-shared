27a60d74c4399ba1302a4b9b3d0be2bc2b969bbd
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-09-21 11:31:01 +0200

    Define SelectOption internal component before Select one

--- src/components/input/SelectNext.tsx
+++ src/components/input/SelectNext.tsx
@@ -16,127 +16,15 @@ import { MenuCollapseIcon, MenuExpandIcon } from '../icons';
 import Button from './Button';
 import SelectContext from './SelectContext';
 
-export type SelectProps<T> = {
-  value: T;
-  onChange: (newValue: T) => void;
-  label: ComponentChildren;
-  children: ComponentChildren;
-  disabled?: boolean;
+export type SelectOptionStatus = {
+  selected: boolean;
+  disabled: boolean;
 };
-
-function SelectMain<T>({
-  label,
-  value,
-  onChange,
-  children,
-  disabled,
-}: SelectProps<T>) {
-  const [isListboxOpen, setIsListboxOpen] = useState(false);
-  const closeListbox = useCallback(() => setIsListboxOpen(false), []);
-  const wrapperRef = useRef<HTMLDivElement | null>(null);
-  const listboxRef = useRef<HTMLDivElement | null>(null);
-  const listboxId = useId();
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-  const buttonId = useId();
-  const [shouldDropUp, setShouldDropUp] = useState(false);
-
-  const selectValue = useCallback(
-    (newValue: unknown) => {
-      onChange(newValue as T);
-      closeListbox();
-    },
-    [closeListbox, onChange],
-  );
-
-  // When clicking away or pressing `Esc`, close the listbox
-  useClickAway(wrapperRef, closeListbox);
-  useKeyPress(['Escape'], closeListbox);
-
-  // Vertical arrow key for options in the listbox
-  useArrowKeyNavigation(wrapperRef, { horizontal: false });
-
-  useLayoutEffect(() => {
-    if (!isListboxOpen) {
-      // Focus button after closing listbox
-      buttonRef.current!.focus();
-      // Reset shouldDropUp so that it does not affect calculations next time
-      // it opens
-      setShouldDropUp(false);
-    } else {
-      const viewportHeight = window.innerHeight;
-      const { top: buttonDistanceToTop, bottom: buttonBottom } =
-        buttonRef.current!.getBoundingClientRect();
-      const buttonDistanceToBottom = viewportHeight - buttonBottom;
-      const { bottom: listboxBottom } =
-        listboxRef.current!.getBoundingClientRect();
-      const listboxDistanceToBottom = viewportHeight - listboxBottom;
-
-      // The listbox should drop up only if there's not enough space below it for
-      // the listbox max height, and there's also more space above
-      setShouldDropUp(
-        listboxDistanceToBottom < 0 &&
-          buttonDistanceToTop > buttonDistanceToBottom,
-      );
-    }
-  }, [isListboxOpen]);
-
-  return (
-    <div className="relative" ref={wrapperRef}>
-      <Button
-        id={buttonId}
-        variant="custom"
-        classes={classnames(
-          'w-full flex border',
-          'bg-grey-0 disabled:bg-grey-1 disabled:text-grey-6',
-        )}
-        expanded={isListboxOpen}
-        pressed={isListboxOpen}
-        disabled={disabled}
-        aria-haspopup="listbox"
-        aria-controls={listboxId}
-        elementRef={buttonRef}
-        onClick={() => setIsListboxOpen(prev => !prev)}
-        onKeyDown={e => {
-          if (e.key === 'ArrowDown' && !isListboxOpen) {
-            setIsListboxOpen(true);
-          }
-        }}
-      >
-        {label}
-        <div className="grow" />
-        {isListboxOpen ? <MenuCollapseIcon /> : <MenuExpandIcon />}
-      </Button>
-      <SelectContext.Provider value={{ selectValue, value }}>
-        <div
-          className={classnames(
-            'absolute z-5 w-full max-h-80 overflow-y-auto',
-            'rounded border bg-white shadow hover:shadow-md focus-within:shadow-md',
-            {
-              'top-full mt-1': !shouldDropUp,
-              'bottom-full mb-1': shouldDropUp,
-              hidden: !isListboxOpen,
-            },
-          )}
-          role="listbox"
-          ref={listboxRef}
-          id={listboxId}
-          aria-labelledby={buttonId}
-          aria-orientation="vertical"
-        >
-          {children}
-        </div>
-      </SelectContext.Provider>
-    </div>
-  );
-}
 
 export type SelectOptionProps<T> = {
   value: T;
   disabled?: boolean;
-  children: (status: {
-    selected: boolean;
-    disabled: boolean;
-  }) => ComponentChildren;
+  children: (status: SelectOptionStatus) => ComponentChildren;
 };
 
 function SelectOption<T>({
@@ -177,6 +65,120 @@ function SelectOption<T>({
         {children({ selected, disabled })}
       </div>
     </Button>
+  );
+}
+
+export type SelectProps<T> = {
+  value: T;
+  onChange: (newValue: T) => void;
+  label: ComponentChildren;
+  children: ComponentChildren;
+  disabled?: boolean;
+};
+
+function SelectMain<T>({
+  label,
+  value,
+  onChange,
+  children,
+  disabled,
+}: SelectProps<T>) {
+  const [isListboxOpen, setIsListboxOpen] = useState(false);
+  const [shouldListboxDropUp, setShouldListboxDropUp] = useState(false);
+  const closeListbox = useCallback(() => setIsListboxOpen(false), []);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const listboxRef = useRef<HTMLDivElement | null>(null);
+  const listboxId = useId();
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const buttonId = useId();
+
+  const selectValue = useCallback(
+    (newValue: unknown) => {
+      onChange(newValue as T);
+      closeListbox();
+    },
+    [closeListbox, onChange],
+  );
+
+  // When clicking away or pressing `Esc`, close the listbox
+  useClickAway(wrapperRef, closeListbox);
+  useKeyPress(['Escape'], closeListbox);
+
+  // Vertical arrow key for options in the listbox
+  useArrowKeyNavigation(wrapperRef, { horizontal: false });
+
+  useLayoutEffect(() => {
+    if (!isListboxOpen) {
+      // Focus button after closing listbox
+      buttonRef.current!.focus();
+      // Reset shouldDropUp so that it does not affect calculations next time
+      // it opens
+      setShouldListboxDropUp(false);
+    } else {
+      const viewportHeight = window.innerHeight;
+      const { top: buttonDistanceToTop, bottom: buttonBottom } =
+        buttonRef.current!.getBoundingClientRect();
+      const buttonDistanceToBottom = viewportHeight - buttonBottom;
+      const { bottom: listboxBottom } =
+        listboxRef.current!.getBoundingClientRect();
+      const listboxDistanceToBottom = viewportHeight - listboxBottom;
+
+      // The listbox should drop up only if there's not enough space below it for
+      // the listbox max height, and there's also more space above
+      setShouldListboxDropUp(
+        listboxDistanceToBottom < 0 &&
+          buttonDistanceToTop > buttonDistanceToBottom,
+      );
+    }
+  }, [isListboxOpen]);
+
+  return (
+    <div className="relative" ref={wrapperRef}>
+      <Button
+        id={buttonId}
+        variant="custom"
+        classes={classnames(
+          'w-full flex border',
+          'bg-grey-0 disabled:bg-grey-1 disabled:text-grey-6',
+        )}
+        expanded={isListboxOpen}
+        pressed={isListboxOpen}
+        disabled={disabled}
+        aria-haspopup="listbox"
+        aria-controls={listboxId}
+        elementRef={buttonRef}
+        onClick={() => setIsListboxOpen(prev => !prev)}
+        onKeyDown={e => {
+          if (e.key === 'ArrowDown' && !isListboxOpen) {
+            setIsListboxOpen(true);
+          }
+        }}
+      >
+        {label}
+        <div className="grow" />
+        {isListboxOpen ? <MenuCollapseIcon /> : <MenuExpandIcon />}
+      </Button>
+      <SelectContext.Provider value={{ selectValue, value }}>
+        <div
+          className={classnames(
+            'absolute z-5 w-full max-h-80 overflow-y-auto',
+            'rounded border bg-white shadow hover:shadow-md focus-within:shadow-md',
+            {
+              'top-full mt-1': !shouldListboxDropUp,
+              'bottom-full mb-1': shouldListboxDropUp,
+              hidden: !isListboxOpen,
+            },
+          )}
+          role="listbox"
+          ref={listboxRef}
+          id={listboxId}
+          aria-labelledby={buttonId}
+          aria-orientation="vertical"
+        >
+          {children}
+        </div>
+      </SelectContext.Provider>
+    </div>
   );
 }
 
