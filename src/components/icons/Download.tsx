83f2875c495dd27aacb7c4e0619c843256a4624c
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-03-21 12:40:29 +0100

    Add download icon

---
+++ src/components/icons/Download.tsx
@@ -0,0 +1,25 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type DownloadIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from download.svg
+ */
+export default function DownloadIcon(props: DownloadIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      data-component="DownloadIcon"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m8.707 11.707-.094.083-.102.07a1 1 0 0 1-1.218-.153l-4-4-.083-.094a1 1 0 0 1 .083-1.32l.094-.083a1 1 0 0 1 1.32.083L7 8.585V1l.007-.117A1 1 0 0 1 9 1v7.585l2.293-2.292.094-.083a1 1 0 0 1 1.32 1.497zm7.286-1.824A1 1 0 0 0 15 9l-.117.007A1 1 0 0 0 14 10v4H2v-4l-.007-.117A1 1 0 0 0 0 10v5l.007.117A1 1 0 0 0 1 16h14l.117-.007A1 1 0 0 0 16 15v-5z"
+      />
+    </svg>
+  );
+}
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -26,6 +26,7 @@ export { default as CollapseIcon } from './Collapse';
 export { default as ContrastIcon } from './Contrast';
 export { default as CopyIcon } from './Copy';
 export { default as CopyFilledIcon } from './CopyFilled';
+export { default as DownloadIcon } from './Download';
 export { default as EditIcon } from './Edit';
 export { default as EditorLatexIcon } from './EditorLatex';
 export { default as EditorQuoteIcon } from './EditorQuote';
