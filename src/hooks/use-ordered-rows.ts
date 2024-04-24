a1d4291501ca96112b0a54b134540ab34e92b428
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-04-24 10:54:57 +0200

    Fix case-insensitive comparisons in useOrderedRows hook

--- src/hooks/use-ordered-rows.ts
+++ src/hooks/use-ordered-rows.ts
@@ -2,9 +2,13 @@ import { useMemo } from 'preact/hooks';
 
 import type { Order } from '../types';
 
+const collator = new Intl.Collator(undefined, { sensitivity: 'case' });
+
 /**
  * Orders a list of rows based on provided order options.
  * Provided rows are not mutated, but a copy is returned instead.
+ * Strings are compared using `Intl.Collator`, other types are compared using
+ * standard JavaScript comparisons.
  */
 export function useOrderedRows<Row>(
   rows: Row[],
@@ -15,16 +19,18 @@ export function useOrderedRows<Row>(
       return rows;
     }
 
-    return [...rows].sort((a, b) => {
-      if (a[order.field] === b[order.field]) {
+    return [...rows].sort(({ [order.field]: a }, { [order.field]: b }) => {
+      const [x, y] = order.direction === 'ascending' ? [a, b] : [b, a];
+
+      if (typeof x === 'string' && typeof y === 'string') {
+        return collator.compare(x, y);
+      }
+
+      if (x === y) {
         return 0;
       }
 
-      if (order.direction === 'ascending') {
-        return a[order.field] > b[order.field] ? 1 : -1;
-      }
-
-      return a[order.field] > b[order.field] ? -1 : 1;
+      return x > y ? 1 : -1;
     });
   }, [order, rows]);
 }
