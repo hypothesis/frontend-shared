6f011206517e25d99c448562904114e9ebb92005
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-08-08 09:21:21 +0200

    Add orderable icon

---
+++ src/components/icons/Orderable.tsx
@@ -0,0 +1,28 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type OrderableIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from orderable.svg
+ */
+export default function OrderableIcon(props: OrderableIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+      data-component="OrderableIcon"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M4.293 5.293a1 1 0 0 0 1.414 1.414L8 4.414l2.293 2.293a1 1 0 1 0 1.414-1.414l-3-3a1 1 0 0 0-1.414 0zm0 5.414a1 1 0 0 1 1.414-1.414L8 11.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
