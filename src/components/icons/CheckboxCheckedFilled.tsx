6b4177e1def12d12e5b2f5cca692b50b0b907608
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-08-19 13:00:28 +0200

    Add checkboxes to multi-select options

---
+++ src/components/icons/CheckboxCheckedFilled.tsx
@@ -0,0 +1,29 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type CheckboxCheckedFilledIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from checkbox-checked-filled.svg
+ */
+export default function CheckboxCheckedFilledIcon(
+  props: CheckboxCheckedFilledIconProps,
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+      data-component="CheckboxCheckedFilledIcon"
+      {...props}
+    >
+      <rect width="16" height="16" fill="currentColor" rx="4" />
+      <path
+        fill="#fff"
+        d="M11.136 3.82a.75.75 0 0 1 1.278.78l-.05.08-5.25 7.5a.75.75 0 0 1-1.204.033l-.053-.077-2.25-3.75a.75.75 0 0 1 1.237-.844l.05.072 1.654 2.758z"
+      />
+    </svg>
+  );
+}
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -20,6 +20,7 @@ export { default as CcZeroFilledIcon } from './CcZeroFilled';
 export { default as CheckIcon } from './Check';
 export { default as CheckboxIcon } from './Checkbox';
 export { default as CheckboxCheckedIcon } from './CheckboxChecked';
+export { default as CheckboxCheckedFilledIcon } from './CheckboxCheckedFilled';
 export { default as CheckboxOutlineIcon } from './CheckboxOutline';
 export { default as CodeIcon } from './Code';
 export { default as CollapseIcon } from './Collapse';
