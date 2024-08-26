c7e9a82f0f212c8e13a0a761f9f727eb47f8eea7
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-08-26 11:03:39 +0200

    Add new icons for radio button

---
+++ src/components/icons/Radio.tsx
@@ -0,0 +1,31 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type RadioIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from radio.svg
+ */
+export default function RadioIcon(props: RadioIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+      data-component="RadioIcon"
+      {...props}
+    >
+      <rect width="15" height="15" x=".5" y=".5" fill="#FAFAFA" rx="7.5" />
+      <rect
+        width="15"
+        height="15"
+        x=".5"
+        y=".5"
+        stroke="currentColor"
+        rx="7.5"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/RadioChecked.tsx
@@ -0,0 +1,32 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type RadioCheckedIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from radio-checked.svg
+ */
+export default function RadioCheckedIcon(props: RadioCheckedIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+      data-component="RadioCheckedIcon"
+      {...props}
+    >
+      <rect width="15" height="15" x=".5" y=".5" fill="#FAFAFA" rx="7.5" />
+      <rect
+        width="15"
+        height="15"
+        x=".5"
+        y=".5"
+        stroke="currentColor"
+        rx="7.5"
+      />
+      <rect width="10" height="10" x="3" y="3" fill="currentColor" rx="5" />
+    </svg>
+  );
+}
