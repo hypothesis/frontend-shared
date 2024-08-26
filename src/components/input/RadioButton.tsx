8a22c5785d991215511f1e318eab9b8ad73117a9
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-08-26 13:50:16 +0200

    Add RadioButton component

--- src/components/input/RadioButton.tsx
+++ src/components/input/RadioButton.tsx
@@ -6,7 +6,6 @@ import ToggleInput from './ToggleInput';
 
 type ComponentProps = {
   checked?: boolean;
-
   /** Custom icon to show when input is unchecked */
   icon?: IconComponent;
   /** Custom icon to show when input is checked */
