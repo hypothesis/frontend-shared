5946bead6ee18c2d0184d605e960ca9cdb1ea040
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-09-06 10:00:58 +0200

    Add new FilterClearIcon

---
+++ src/components/icons/FilterClear.tsx
@@ -0,0 +1,32 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type FilterClearIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from filter-clear.svg
+ */
+export default function FilterClearIcon(props: FilterClearIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+      data-component="FilterClearIcon"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m8.4 12 1.428 2H5a1 1 0 0 1-.117-1.993L5 12zM11.172 9H13l.117-.007A1 1 0 0 0 13 7H9.744zM4.828 7l1.428 2H3a1 1 0 0 1-.117-1.993L3 7zM7.6 4H15l.117-.007A1 1 0 0 0 15 2H6.172zM1.268 2q.048.083.105.162L2.685 4H1a1 1 0 0 1-.117-1.993L1 2z"
+      />
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M13.581 15.814a1 1 0 0 0 .233-1.395l-10-14A1 1 0 1 0 2.186 1.58l10 14a1 1 0 0 0 1.395.233"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
