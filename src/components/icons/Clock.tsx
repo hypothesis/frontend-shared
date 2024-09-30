2f4d9d5540e24c0ae63db61644cd9be44cf31520
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-09-30 11:42:10 +0200

    Add new clock icon

---
+++ src/components/icons/Clock.tsx
@@ -0,0 +1,34 @@
+// This file was auto-generated using scripts/generate-icons.js
+import type { JSX } from 'preact';
+
+export type ClockIconProps = JSX.SVGAttributes<SVGSVGElement>;
+
+/**
+ * Icon generated from clock.svg
+ */
+export default function ClockIcon(props: ClockIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 12 12"
+      data-component="ClockIcon"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M6 0c3.04 0 6 2.96 6 6s-2.96 6-6 6-6-2.96-6-6 2.96-6 6-6m0 1.5C3.79 1.5 1.5 3.79 1.5 6s2.29 4.5 4.5 4.5 4.5-2.29 4.5-4.5S8.21 1.5 6 1.5"
+        clip-rule="evenodd"
+      />
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M6 2.25a.75.75 0 0 1 .75.75v2.599l1.916 1.277a.75.75 0 0 1-.832 1.248l-2.25-1.5A.75.75 0 0 1 5.25 6V3A.75.75 0 0 1 6 2.25"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
