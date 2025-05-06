5978d939a8b48c6ddc1c134a83f62f2d629c6897
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-05-06 10:59:36 +0200

    Update hypothesis frontend build and eslint config packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
 import hypothesisBase from 'eslint-config-hypothesis/base';
 import hypothesisJSX from 'eslint-config-hypothesis/jsx';
 import hypothesisTS from 'eslint-config-hypothesis/ts';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 
-export default [
-  {
-    ignores: ['.yalc/**', 'lib/**', 'build/**'],
-  },
+export default defineConfig(
+  globalIgnores(['.yalc/**', 'lib/**', 'build/**']),
 
-  ...hypothesisBase,
-  ...hypothesisJSX,
-  ...hypothesisTS,
+  hypothesisBase,
+  hypothesisJSX,
+  hypothesisTS,
 
   // Icons
   {
@@ -32,4 +31,4 @@ export default [
       },
     },
   },
-];
+);
